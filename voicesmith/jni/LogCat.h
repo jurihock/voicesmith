7190a1c9c180202229357c8a94b30e2a25672376
Juergen Hock <juergen.hock@jurihock.de>
2012-06-17 23:12:31 +0200

    Some changes made.

--- voicesmith/jni/LogCat.h
+++ voicesmith/jni/LogCat.h
@@ -1,10 +1,11 @@
-// Be sure to include the llog library in Android.mk
+// Be sure to include the LogCat library in Android.mk
 // Log example: LOG("FFT size %i", fft->size);
 
 #ifndef LogCat_h
 #define LogCat_h
 
 #include <android/log.h>
+
 #define LOG(message, args...) __android_log_print(ANDROID_LOG_DEBUG, "Voicesmith", message, args)
 
 #endif
