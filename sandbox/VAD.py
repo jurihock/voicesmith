7c06ed552b0342cfd1db6fc64c3483e6eea09aff
Juergen Hock <juergen.hock@jurihock.de>
2013-11-08 14:38:42 +0100

    backup latest changes

--- sandbox/VAD.py
+++ sandbox/VAD.py
@@ -3,7 +3,9 @@ from numpy import *
 # See also "Dynamical Energy-Based Speech/Silence Detector for Speech Enhancement Applications" by K. Sakhnov et al.
 
 def vad(signal, windowSize, hopSize, sampleRate, noiseEnergy, hangoverTime):
-    
+
+    isFirstFrame = True
+
     minEnergy = 0
     maxEnergy = 0
 
@@ -22,32 +24,51 @@ def vad(signal, windowSize, hopSize, sampleRate, noiseEnergy, hangoverTime):
 
         # TODO: Check this routine again!
 
-        if (currentEnergy > maxEnergy):
+        if isFirstFrame:
+
+            if currentEnergy > noiseEnergy:
+                maxEnergy = currentEnergy
+                minEnergy = noiseEnergy
+            else:
+                maxEnergy = noiseEnergy * 2
+                minEnergy = currentEnergy
+
+            isFirstFrame = False
+
+        if currentEnergy > maxEnergy:
             maxEnergy = currentEnergy
         
-        if (currentEnergy < minEnergy):
-            minEnergy = currentEnergy
-            minEnergyDelta = 1
+        if currentEnergy < minEnergy:
 
-        if (minEnergy <= noiseEnergy):
-            minEnergy = noiseEnergy
+            if currentEnergy == 0:
+
+                if noiseEnergy >= maxEnergy:
+                    minEnergy = maxEnergy * 0.1
+                else:
+                    minEnergy = noiseEnergy
+
+            else:
+                minEnergy = currentEnergy
+
             minEnergyDelta = 1
 
         _lambda_ = (maxEnergy - minEnergy) / maxEnergy
         threshold = (1 - _lambda_) * maxEnergy + _lambda_ * minEnergy
 
-        if(currentEnergy > threshold):
+        if currentEnergy > threshold:
             hangover = 0
             vadFlags[vadFlagIndex] = 1
-        elif (hangover == hangoverThreshold):
+        elif hangover == hangoverThreshold:
             vadFlags[vadFlagIndex] = 0
         else:
             hangover += 1
             vadFlags[vadFlagIndex] = 1
 
+        # Slightly inrease Emin to prevent low energy anomalies
         minEnergyDelta *= 1.0001
         minEnergy *= minEnergyDelta
 
+        #vadFlags[vadFlagIndex] = threshold # TEST
         vadFlagIndex += 1
         
     return vadFlags
