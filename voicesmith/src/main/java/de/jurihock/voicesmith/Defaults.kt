869fff2a24f33e01fd799e29881eb999e63e093b
Jürgen Hock <juergen.hock@jurihock.de>
2024-06-05 00:23:54 +0200

    Set default samplerate to 48000 Hz

--- voicesmith/src/main/java/de/jurihock/voicesmith/Defaults.kt
+++ voicesmith/src/main/java/de/jurihock/voicesmith/Defaults.kt
@@ -3,6 +3,6 @@ package de.jurihock.voicesmith
 object Defaults {
   const val INPUT: Int = 0
   const val OUTPUT: Int = 0
-  const val SAMPLERATE: Int = 0
+  const val SAMPLERATE: Int = 48000
   const val BUFFERSIZE: Int = 64
 }
