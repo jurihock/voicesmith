d4a1a64ae180bb89b989ecf6be252f37d9e99295
Jürgen Hock <juergen.hock@jurihock.de>
2024-06-05 21:18:27 +0200

    Enable float precision by default, since pffft doesn't support double precision

--- voicesmith/src/main/cpp/de/jurihock/voicesmith/fx/PitchTimbreShiftEffect.h
+++ voicesmith/src/main/cpp/de/jurihock/voicesmith/fx/PitchTimbreShiftEffect.h
@@ -19,6 +19,8 @@ public:
 
 private:
 
+  typedef float stft_t;
+
   struct {
     float samplerate;
     size_t blocksize;
@@ -29,13 +31,13 @@ private:
   } config;
 
   struct {
-    std::vector<double> input;
-    std::vector<double> output;
+    std::vector<stft_t> input;
+    std::vector<stft_t> output;
   } buffer;
 
   std::shared_ptr<FFT> fft;
-  std::unique_ptr<stftpitchshift::STFT<double>> stft;
-  std::unique_ptr<stftpitchshift::StftPitchShiftCore<double>> core;
+  std::unique_ptr<stftpitchshift::STFT<stft_t>> stft;
+  std::unique_ptr<stftpitchshift::StftPitchShiftCore<stft_t>> core;
 
   template<typename X, typename Y>
   inline static Y transform(const X x) { return static_cast<Y>(x); }
